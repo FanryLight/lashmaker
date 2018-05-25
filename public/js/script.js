02f2eeb87e0d4420f4090167e65814a4e5c48659
fanry <medoeva@brights.com.ua>
2018-05-25 17:00:42 +0300

    update

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+jQuery(function ($) {
+    $("#phone").mask("(999) 999-9999", {placeholder: "(___) ___-____"});
+
+    $('#sendButton').on('click', function (event) {
+        event.preventDefault();
+        const name = $('#name').val();
+        const phone = $('#phone').val();
+        let flag = true;
+        if (!name) {
+            $('#name').parent().addClass('has-error');
+            flag = false;
+        }
+        if (!phone || !(/^\([0-9]{3}\) [0-9]{3}\-[0-9]{4}$/).test(phone)) {
+            $('#phone').parent().addClass('has-error');
+            flag = false;
+        }
+        if (flag) {
+            $.ajax(
+                {
+                    url: '/sendContacts',
+                    data: {
+                        'name': name,
+                        'phone': phone
+                    },
+                    success: function (data) {
+                        console.log(data);
+                    }
+                }
+            )
+        }
+    });
+});
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
@@ -13,5 +15,31 @@ class MainController extends Controller
      */
     public function indexAction() {
         return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/sendContacts")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function sendContactsAction(Request $request) {
+        $name = $request->get('name');
+        $phone = $request->get('phone');
+        if ($name && $phone) {
+            $message = (new \Swift_Message('Запись на процедуру'))
+                ->setFrom('medoeva@brights.com.ua')
+                ->setTo('fanrylight@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'email.html.twig',
+                        [
+                            'name' => $name,
+                            'phone' => $phone
+                        ]
+                    ),
+                    'text/plain'
+                );
+            $this->get('mailer')->send($message);
+        }
+        return new JsonResponse();
     }
 }
